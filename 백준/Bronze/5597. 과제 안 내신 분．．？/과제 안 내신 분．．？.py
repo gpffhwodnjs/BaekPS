885d200d22c20bf170b596722a03b2be6694ca34
정재원 <104828805+gpffhwodnjs@users.noreply.github.com>
2023-08-11 00:20:01 +0900

    Update 과제 안 내신 분．．？.py

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.py
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.py
@@ -3,6 +3,10 @@ students = []
 for i in range(1,31) :
     students.append(i)
 
+#더 간단한 방법
+# 1. students = list(range(1,31))
+# 2. students = [i for i in range(1,31)]
+
 while True :
     n = int(input())
     students.remove(n)
